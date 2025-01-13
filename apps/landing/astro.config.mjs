704335725b11e1e00f61a5a0562f9f12b166c900
mhhnn <hariharanithin@gmail.com>
2025-01-13 08:08:25 -0500

    v1 code

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,15 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'static',
-  outDir: 'dist',
-  publicDir: 'public',
-  root: '.',
-  build: {
-    assets: 'assets'
-  },
-  base: '/'
-});
+export default defineConfig({});
