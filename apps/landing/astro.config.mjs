ef760989c6a849a6cab68ac5ead0b70c99b550da
mhhnn <hariharanithin@gmail.com>
2025-01-13 21:12:51 -0500

    v1 code

--- apps/landing/astro.config.mjs
+++ apps/landing/astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   outDir: './dist',
+  adapter: vercel(),
   build: {
     assets: 'assets'
   },
